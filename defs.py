b3dab009c53907a7935cc1abbe177460c7dca278
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2026-05-20 10:23:47 +0200

    Initial project commit

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = 'ccfefa09d7a4e9cf7340e223baec347d-6b7b8987841894f4b659e2d24b192c74'
+ACCOUNT_ID = '101-001-39358426-001'
+OANDA_URL = f'https://api-fxpractice.oanda.com/v3/accounts/{ACCOUNT_ID}'
+
+SECURE_HEADER = {
+               'Authorization': f'Bearer {API_KEY}'
+}
